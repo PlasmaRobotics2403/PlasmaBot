0f47808ef7d8f1a36044a712b445dcd6023e1a8e
A Detection Algorithm (ADA) <plasma2403@gmail.com>
2016-06-13 10:50:57 -0700

    Revert "pls"
    This reverts commit 01d09bb9bd8087840f9841be09e9de749eff1884.

--- plasmaBot/plugins/__init__.py
+++ plasmaBot/plugins/__init__.py
@@ -1,5 +1,3 @@
-import os
-
 all_commands = {}
 
 def command(name):
@@ -13,12 +11,5 @@ def command(name):
 
 # import plasmaBot.plugins.meta
 # import plasmaBot.plugins.moderation
-# import plasmaBot.plugins.music
+import plasmaBot.plugins.music
 # import plasmaBot.plugins.server
-
-for name in os.listdir("plasmaBot.plugins"):
-    if name.endswith(".py"):
-          #strip the extension
-         module = name[:-3]
-         # set the module name in the current global name space:
-         globals()[module] = __import__(os.path.join("plugins", name)
