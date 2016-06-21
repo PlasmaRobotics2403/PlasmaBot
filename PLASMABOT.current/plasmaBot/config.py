ea880cda82d2f351886e88dfe4a751d9478d5ba6
A Detection Algorithm (ADA) <Adetectionalgorithmbot@gmail.com>
2016-06-21 16:31:21 -0700

    continued work on config.py

--- PLASMABOT.current/plasmaBot/config.py
+++ PLASMABOT.current/plasmaBot/config.py
@@ -2,3 +2,18 @@ import os
 import shutil
 import traceback
 import configparser
+
+from .exceptions import HelpfulError
+
+class Config:
+    def __init__(self, config_file):
+        self.config_file = config_file
+        config = configparser.ConfigParser()
+
+        if not config.read(config_file, encoding='utf-8'):
+            print('[PB][CONFIG] Config file not found, copying example_options.')
+
+            try:
+                shutil.copy('plasmaBot/defaults/example_options.ini', config_file)
+
+                
