a38bf49b4846f98fd770faabf5a1a3c27190f37c
A Detection Algorithm (ADA) <Adetectionalgorithmbot@gmail.com>
2016-06-21 16:30:47 -0700

    updated bot.py

--- PLASMABOT.current/plasmaBot/bot.py
+++ PLASMABOT.current/plasmaBot/bot.py
@@ -1,1 +1,13 @@
-# Initial File for directory and file setup.  Will be coded externally.
+import os
+import sys
+import time
+import shutil
+import traceback
+import discord
+
+class PlasmaBot(discord.Client)
+    def __init__(self):
+
+    def run(self):
+        try:
+            self.loop.run_until_complete(self.start(*self.config.auth))
