12e195e239a01c0d4aaef41cb4d78d71c782e4a4
A Detection Algorithm <plasma2403@gmail.com>
2016-08-03 14:28:23 -0700

    Added regular expressions to TBA command to allow for raw keys in commands instead of manual lookups if desired.

--- PLASMABOT.current/plasmaBot/plugins/TBA.py
+++ PLASMABOT.current/plasmaBot/plugins/TBA.py
@@ -4,6 +4,8 @@ import discord
 from plasmaBot import exceptions
 import discord
 
+import re
+
 from plasmaBot.plugins.resources.TBAPythonAPI import *
 
 import logging
@@ -17,6 +19,8 @@ class TBAPlugin(PBPlugin):
     def __init__(self, plasmaBot):
         super().__init__(plasmaBot)
         self.TBA = TBAParser(2403, "Discord Bot", "0.1.0")
+        self.team_key_pattern = re.compile('[f][r][c][0-9]{3,4}', re.IGNORECASE)
+        self.event_key_pattern = re.compile('[1-2][01289][0-9][0-9][a-z]{3}[a-z0-9]{1,2}', re.IGNORECASE)
 
     async def cmd_tba(self, message, channel, leftover_args):
         """
@@ -40,15 +44,18 @@ class TBAPlugin(PBPlugin):
             return Response(no_response, reply=False, delete_after=45)
 
         if cmd_type == 'team':
-            param = ''
-            for partial_msg in leftover_args:
-                param += partial_msg + ' '
-            param = param[:-1]
-            try:
-                num = int(param)
-            except:
-                return Response('Not a valid team number. ```' + self.bot.config.prefix + 'tba team [Number]```', reply=False, delete_after=30)
-            key = "frc" + str(num)
+            if bool(self.team_key_pattern.search(leftover_args[0])):
+                key = leftover_args[0]
+            else:
+                param = ''
+                for partial_msg in leftover_args:
+                    param += partial_msg + ' '
+                param = param[:-1]
+                try:
+                    num = int(param)
+                except:
+                    return Response('Not a valid team number. `' + self.bot.config.prefix + 'tba team [Number]`', reply=False, delete_after=30)
+                key = "frc" + str(num)
             try:
                 team = self.TBA.get_team(key)
             except:
@@ -63,40 +70,48 @@ class TBAPlugin(PBPlugin):
             return Response(team_data, reply=False, delete_after=60)
 
         elif cmd_type == 'event':
-            try:
-                year_str = leftover_args[0]
-                del leftover_args[0]
-                param = ''
-                for partial_msg in leftover_args:
-                    param += partial_msg + ' '
-                param = param[:-1]
-            except:
-                return Response('Additional arguments needed. ```' + self.bot.config.prefix + 'tba event [Year] [Beginning of event name]```', reply=False, delete_after=30)
-            try:
-                year = int(year_str)
-            except:
-                return Response('Year must be an integer.', reply=False, delete_after=30)
-            #try:
-            key = self.TBA.calc_event_key(year, param)
-            #except:
-            #    return Response('Unknown error looking up event key.', reply=False, delete_after=30)
-            if key == '1':
-                return Response('Multiple events found. Please refine your search.', reply=False, delete_after=30)
-            if key == '0':
-                return Response('No events found. Please ensure spelling is correct.', reply=False, delete_after=30)
+            if bool(self.event_key_pattern.search(leftover_args[0])):
+                key = leftover_args[0]
+
+            else:
+                try:
+                    year_str = leftover_args[0]
+                    del leftover_args[0]
+                    param = ''
+                    for partial_msg in leftover_args:
+                        param += partial_msg + ' '
+                    param = param[:-1]
+                except:
+                    return Response('Additional arguments needed. ```' + self.bot.config.prefix + 'tba event [Year] [Beginning of event name]```', reply=False, delete_after=30)
+                try:
+                    year = int(year_str)
+                except:
+                    return Response('Year must be an integer.', reply=False, delete_after=30)
+
+                key = self.TBA.calc_event_key(year, param)
+
+                if key == '1':
+                    return Response('Multiple events found. Please refine your search.', reply=False, delete_after=30)
+                if key == '0':
+                    return Response('No events found. Please ensure spelling is correct.', reply=False, delete_after=30)
+
             event = self.TBA.get_event(key)
             event_data = event.name + "\nYear: " + str(event.year) + "\nLocation: " + event.location + "\nDates: " + event.start_date + " to " + event.end_date + "\nEvent Type: " + event.event_type_string + "\nhttps://www.thebluealliance.com/event/" + event.key
             return Response(event_data, reply=False, delete_after=60)
 
         elif cmd_type == 'awards':
             if len(leftover_args) >= 1:
-                try:
-                    team_number = int(leftover_args[0])
-                except:
-                    no_response += '\n\nInvalid Team Number Supplied```'
-                    return Response(no_response, reply=False, delete_after=45)
+                if bool(self.team_key_pattern.search(leftover_args[0])):
+                    team_key = leftover_args[0]
+                    team_number = team_key[3:]
+                else:
+                    try:
+                        team_number = int(leftover_args[0])
+                    except:
+                        no_response += '\n\nInvalid Team Number Supplied```'
+                        return Response(no_response, reply=False, delete_after=45)
 
-                team_key = 'frc' + str(team_number)
+                    team_key = 'frc' + str(team_number)
 
                 if len(leftover_args) >= 2:
                     try:
