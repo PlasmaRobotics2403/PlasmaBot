4d9a822ee2b8b3b9aa2b97da1508a4cdd9464b3f
EntropyValley <github@itsval.org>
2024-12-29 14:42:18 -0800

    Add initial Game Guess Cog

---
+++ plasmaBot/cogs/guesser.py
@@ -0,0 +1,216 @@
+import peewee
+
+import discord
+
+from plasmaBot.cog import PlasmaCog, chat_group, chat_command
+from plasmaBot.database import aio_first
+
+class Guesser(PlasmaCog):
+    """Game Guess Cog"""
+
+    @chat_command(name='guess', help='Get added to the Guesser Channel')
+    async def guess(self, ctx):
+        """Get added to the Guesser Channel"""
+        GuesserSettings = self.tables.GuesserSettings
+        settings = await aio_first(GuesserSettings.select().where(GuesserSettings.guild_id == str(ctx.guild.id)))
+
+        if not settings:
+            settings = GuesserSettings(guild_id=str(ctx.guild.id))
+            await settings.aio_save()
+
+        if not settings.enabled:
+            await ctx.send('The Guesser Game is not enabled', ephemeral=True)
+            return
+
+        if not settings.guesser_channel:
+            await ctx.send('The Guesser Channel is not set', ephemeral=True)
+            return
+
+        if not settings.guesser_role:
+            await ctx.send('The Guesser Role is not set', ephemeral=True)
+            return
+
+        role = ctx.guild.get_role(int(settings.guesser_role))
+
+        if not role:
+            await ctx.send('The Guesser Role is not valid', ephemeral=True)
+            return
+
+        if role in ctx.author.roles:
+            await ctx.send('You are already in the Guesser Channel', ephemeral=True)
+            return
+
+        await ctx.author.add_roles(role, reason="Guesser Guess Added")
+
+        await ctx.send('You have been added to the Guesser Channel', ephemeral=True)
+
+    @chat_group(name='config_guesser', help='Configure the Guesser Game')
+    async def config_guesser(self, ctx):
+        """Configure the Guesser Game"""
+        pass
+
+    @config_guesser.command(name='list_settings', help='List the Guesser Game Settings')
+    async def list_settings(self, ctx):
+        """List the Guesser Game Settings"""
+        if not (ctx.author.guild_permissions.manage_guild or ctx.author.id in self.bot.developers):
+            await ctx.send('You must have `Manage Server` permissions to use this command', ephemeral=True)
+            return
+        
+        GuesserSettings = self.tables.GuesserSettings
+        settings = await aio_first(GuesserSettings.select().where(GuesserSettings.guild_id == str(ctx.guild.id)))
+
+        if not settings:
+            settings = GuesserSettings(guild_id=str(ctx.guild.id))
+            await settings.aio_save()
+        
+        await ctx.send(f"Enabled: {settings.enabled}\nGuesser Channel: {settings.guesser_channel}\nGuesser Role: {settings.guesser_role}\nGuesser Message: {settings.guesser_message}")
+
+    @config_guesser.command(name='toggle', help='Toggle the Guesser Game')
+    async def toggle(self, ctx):
+        """Toggle the Guesser Game"""
+        if not (ctx.author.guild_permissions.manage_guild or ctx.author.id in self.bot.developers):
+            await ctx.send('You must have `Manage Server` permissions to use this command', ephemeral=True)
+            return
+        
+        GuesserSettings = self.tables.GuesserSettings
+        settings = await aio_first(GuesserSettings.select().where(GuesserSettings.guild_id == str(ctx.guild.id)))
+
+        if not settings:
+            settings = GuesserSettings(guild_id=str(ctx.guild.id))
+            await settings.aio_save()
+        
+        settings.enabled = not settings.enabled
+        await settings.aio_save()
+
+        await ctx.send(f"Guesser Game {'Enabled' if settings.enabled else 'Disabled'}")
+
+    @config_guesser.command(name='set_channel', help='Set the Guesser Channel')
+    async def set_channel(self, ctx, channel: discord.TextChannel):
+        """Set the Guesser Channel"""
+        if not (ctx.author.guild_permissions.manage_guild or ctx.author.id in self.bot.developers):
+            await ctx.send('You must have `Manage Server` permissions to use this command', ephemeral=True)
+            return
+        
+        GuesserSettings = self.tables.GuesserSettings
+        settings = await aio_first(GuesserSettings.select().where(GuesserSettings.guild_id == str(ctx.guild.id)))
+
+        if not settings:
+            settings = GuesserSettings(guild_id=str(ctx.guild.id))
+            await settings.aio_save()
+        
+        settings.guesser_channel = str(channel.id)
+        await settings.aio_save()
+
+        await ctx.send(f"Guesser Channel set to {channel.mention}")
+
+    @config_guesser.command(name='set_role', help='Set the Guesser Role')
+    async def set_role(self, ctx, role: discord.Role):
+        """Set the Guesser Role"""
+        if not (ctx.author.guild_permissions.manage_guild or ctx.author.id in self.bot.developers):
+            await ctx.send('You must have `Manage Server` permissions to use this command', ephemeral=True)
+            return
+        
+        GuesserSettings = self.tables.GuesserSettings
+        settings = await aio_first(GuesserSettings.select().where(GuesserSettings.guild_id == str(ctx.guild.id)))
+
+        if not settings:
+            settings = GuesserSettings(guild_id=str(ctx.guild.id))
+            await settings.aio_save()
+        
+        settings.guesser_role = str(role.id)
+        await settings.aio_save()
+
+        await ctx.send(f"Guesser Role set to @{role.name} ({role.id})")
+
+    @config_guesser.command(name='set_message', help='Set the Guesser Message')
+    async def set_message(self, ctx, *, message: str):
+        """Set the Guesser Message"""
+        if not (ctx.author.guild_permissions.manage_guild or ctx.author.id in self.bot.developers):
+            await ctx.send('You must have `Manage Server` permissions to use this command', ephemeral=True)
+            return
+        
+        GuesserSettings = self.tables.GuesserSettings
+        settings = await aio_first(GuesserSettings.select().where(GuesserSettings.guild_id == str(ctx.guild.id)))
+
+        if not settings:
+            settings = GuesserSettings(guild_id=str(ctx.guild.id))
+            await settings.aio_save()
+        
+        settings.guesser_message = message
+        await settings.aio_save()
+
+        await ctx.send(f"Guesser Message set to:\n{message}")
+
+    @PlasmaCog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        
+        if not message.guild:
+            return
+        
+        GuesserSettings = self.tables.GuesserSettings
+        settings = await aio_first(GuesserSettings.select().where(GuesserSettings.guild_id == str(message.guild.id)))
+
+        if not settings:
+            return
+        
+        if not settings.enabled:
+            return
+        
+        if not settings.guesser_channel:
+            return
+        
+        if message.channel.id != int(settings.guesser_channel):
+            return
+        
+        if not settings.guesser_role:
+            return
+        
+        role = message.guild.get_role(int(settings.guesser_role))
+
+        if not role:
+            return
+        
+        if role not in message.author.roles:
+            await message.delete()
+            return
+        
+        await message.author.remove_roles(role, reason="Guesser Guess Logged")
+
+        await message.add_reaction("✅")
+
+        if settings.guesser_message_id:
+            try:
+                msg = await message.channel.fetch_message(int(settings.guesser_message_id))        
+                await msg.delete()
+            except:
+                pass
+            
+        if settings.guesser_message:
+            embed = discord.Embed(description=settings.guesser_message)
+            msg = await message.channel.send(embed=embed)
+            settings.guesser_message_id = str(msg.id)
+            await settings.aio_save()
+
+
+async def setup(bot):
+    new_cog = Guesser(bot)
+
+    class GuesserSettings(bot.database.base_model):
+        """Represents a Guild's Guesser Settings"""
+        db_id = peewee.AutoField(primary_key=True)
+        guild_id = peewee.TextField()
+        enabled = peewee.BooleanField(default=False)
+        guesser_channel = peewee.TextField(null=True)
+        guesser_role = peewee.TextField(null=True)
+        guesser_message = peewee.TextField(null=True)
+        guesser_message_id = peewee.TextField(null=True)
+
+    new_cog.register_tables(
+        [
+            GuesserSettings
+        ]
+    )
+
+    await bot.add_cog(new_cog)
